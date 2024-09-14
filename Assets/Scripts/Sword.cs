2cff1382ac1cee0ea144b33912afdcde5c5f7671
FaithintheAbyss <yinyang1520@gmail.com>
2024-09-14 14:05:50 +0700

    add slash anim prefab

--- Assets/Scripts/Sword.cs
+++ Assets/Scripts/Sword.cs
@@ -4,15 +4,53 @@ using UnityEngine;
 
 public class Sword : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private PlayerControls playerControls;
+    private Animator myAnimator;
+    private Player player;
+    private ActiveWeapon activeWeapon;
+
+
+
+    private void Awake()
+    {
+        myAnimator = GetComponent<Animator>();
+        playerControls = new PlayerControls();
+
+        activeWeapon = GetComponentInParent<ActiveWeapon>();
+        player = GetComponentInParent<Player>();
+    }
+    private void OnEnable()
+    {
+        playerControls.Enable();
+    }
     void Start()
     {
-        
+
+        playerControls.Combat.Attack.started += _ => Attack();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update() {
+        MouseFollowWithOffset();
+    }
+    private void Attack()
     {
+        myAnimator.SetTrigger("Attack");
+    }
+
+    private void MouseFollowWithOffset(){
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 playerScreenPoint = Camera.main.WorldToScreenPoint(player.transform.position);
+
+        // float angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
+
+        if (mousePos.x < playerScreenPoint.x)
+        {
+            activeWeapon.transform.rotation = Quaternion.Euler(0, -180, 0);
+        }
+        else
+        {
+            activeWeapon.transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
         
     }
 }
